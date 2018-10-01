ab0269c2697234113201531661bf26965797be7d
Alfado Puja Kusuma <srontobisnis@gmail.com>
2018-10-01 17:25:25 +0700

    Log Changes 0.0.1.3 + Jwt setting done

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::RegistrationsController < Devise::RegistrationsController
+  protect_from_forgery with: :null_session, only: [:create]
+  respond_to :json
+
+  def create
+    user = CreateUser.new(sign_up_params).call
+    render_resource(user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
+
+  # devise_scope :user do
+  #   post 'api/v1/signup', to: 'api/v1/registrations#create'
+  # end
   root 'item#index'
+
+  namespace :api do
+    namespace :v1 do
+      
+    end
+  end
 end
